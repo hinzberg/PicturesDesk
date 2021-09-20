c88acdae09a3d6a3ed865628832086433abcb5b0
Holger Hinzberg <holger@hinzberg.de>
2021-09-20 20:36:36 +0200

    Initial Commit

---
+++ PicturesDesk/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PicturesDesk
+//
+//  Created by Holger Hinzberg on 20.09.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PicturesDesk/PicturesDeskApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicturesDeskApp.swift
+//  PicturesDesk
+//
+//  Created by Holger Hinzberg on 20.09.21.
+//
+
+import SwiftUI
+
+@main
+struct PicturesDeskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
